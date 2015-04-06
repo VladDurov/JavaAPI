cc81afdf27048919e15d36b0056bf15aba709719
VladislavDurov <durov.vlad@gmail.com>
2015-04-06 12:52:30 +0300

    add example Prototype

---
+++ src/main/java/patterns/prototype/PrototypeDemo.java
@@ -0,0 +1,17 @@
+package patterns.prototype;
+
+public class PrototypeDemo {
+
+    public static void main(String[] args) {
+
+        ShapeCache.loadCache();
+
+
+        for(int i=1; i<3; i++) {
+            Shape clonedShape = ShapeCache.getShape("" + i);
+            clonedShape.draw();
+        }
+
+    }
+
+}
