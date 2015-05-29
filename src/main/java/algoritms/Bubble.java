ac4917a35bdfdaf66c697d2c0c1dcf9e91b7c9de
VladislavDurov <durov.vlad@gmail.com>
2015-05-29 14:41:58 +0300

    add refactor algoritms sorts

--- src/main/java/algoritms/Bubble.java
+++ src/main/java/algoritms/Bubble.java
@@ -5,25 +5,12 @@ public class Bubble {
     public int [] sortUp(int[] arrs){
 
         for (int external = arrs.length; external >=0 ; external--) {
-            innerCicleUp(arrs, external);
+            for (int inner = 1; inner < external; inner++) {
+                swap(arrs, inner);
+            }
         }
         return arrs;
     }
-
-    private void innerCicleUp(int[] arrs, int external) {
-        for (int inner = 1; inner < external; inner++) {
-            swap(arrs, inner);
-        }
-    }
-
-    private void swap(int[] arrs, int inner) {
-        if(arrs[inner-1] > arrs[inner]){
-            int tmp = arrs[inner];
-            arrs[inner] = arrs[inner-1];
-            arrs[inner-1] = tmp;
-        }
-    }
-
 
     public int [] sortDown(int [] arrs){
 
@@ -33,6 +20,15 @@ public class Bubble {
             }
         }
         return arrs;
+    }
+
+
+    private void swap(int[] arrs, int inner) {
+        if(arrs[inner-1] > arrs[inner]){
+            int tmp = arrs[inner];
+            arrs[inner] = arrs[inner-1];
+            arrs[inner-1] = tmp;
+        }
     }
 
 }
